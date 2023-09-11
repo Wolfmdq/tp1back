9a092cd4f47472087d755b10d6ac3e6a93e2bbbb
Wolfmdq <139662630+Wolfmdq@users.noreply.github.com>
2023-09-11 20:12:03 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Tp1 Backend</h1>
+    <?php
+        $saludo = "Hola Mundo";
+        echo $saludo;
+        
+        $par = 10;
+        $impar = 5;
+        print("</br>");
+        echo ($par+$impar);
+        print("</br>");  
+        echo ($par-$impar);
+        print("</br>");
+        echo ($par*$impar);
+        print("</br>");
+        echo ($par/$impar);
+        print("</br>");
+        echo ($par%$impar);
+        print("</br>");
+
+        $Celcius = 20;
+        print("</br>");
+        $Fahrenheit = ($Celcius*1.8)+32;
+        echo $Fahrenheit;
+        print("</br>");
+
+        $base = 18;
+        $altura = 12;
+        print("</br>");
+
+        $perimetro = ($base*$altura);
+        echo $perimetro;
+        print("</br>");
+
+        $radio = 30;
+        echo (2*3.14*$radio);
+        print("</br>");
+
+        echo (3.14*$radio)*(3.14*$radio);
+
+
+
+
+
+        ?>
+    
+</body>
+</html>
